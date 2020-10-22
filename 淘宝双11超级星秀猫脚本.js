d9224661a7349e76c537b7ee8fa140fd576cde9f
hyue418 <admin@huangyue123.com>
2020-10-22 11:02:43 +0800

    新增仿真滑屏动作，避开脚本检测

--- 淘宝双11超级星秀猫脚本.js
+++ 淘宝双11超级星秀猫脚本.js
@@ -3,11 +3,12 @@
  * User: Hyue418
  * Date: 2020/10/21
  * Time: 21:16
- * Versions: 1.0.0
+ * Versions: 1.1.0
  * Github: https://github.com/hyue418
  */
 
 "auto";
+
 console.show();
 height = device.height;
 width = device.width;
@@ -136,17 +137,17 @@ function clickButton(button) {
 
 /**
  * 根据float倍数sleep随机时间
- * @param millisecond 
+ * @param time 
  */
-function randomSleep(millisecond) {
-    sleep(ramdomByFloat(millisecond));
+function randomSleep(time) {
+    sleep(ramdomByFloat(time));
 }
 
 /**
  * 随机滑动
  */
 function randomSwipe() {
-    swipe(ramdomByFloat(width / 2), ramdomByFloat(height / 1.5), ramdomByFloat(width / 2), ramdomByFloat(height / 4), ramdomByFloat(800));
+    smlMove(ramdomByFloat(width / 2), ramdomByFloat(height / 1.5), ramdomByFloat(width / 2), ramdomByFloat(height / 4), ramdomByFloat(800));
 }
 
 /**
@@ -165,3 +166,59 @@ function random(min, max) {
 function ramdomByFloat(number) {
     return random(number, number * float);
 }
+
+/**
+ * 仿真随机带曲线滑动 
+ * @param qx 起点x轴坐标
+ * @param qy 起点y轴坐标
+ * @param zx 终点x轴坐标
+ * @param zy 终点y轴坐标
+ * @param time 滑动时间，毫秒
+ */
+function smlMove(qx, qy, zx, zy, time) {
+    var xxy = [time];
+    var point = [];
+    var dx0 = {
+        "x": qx,
+        "y": qy
+    };
+    var dx1 = {
+        "x": random(qx - 100, qx + 100),
+        "y": random(qy, qy + 50)
+    };
+    var dx2 = {
+        "x": random(zx - 100, zx + 100),
+        "y": random(zy, zy + 50),
+    };
+    var dx3 = {
+        "x": zx,
+        "y": zy
+    };
+    for (var i = 0; i < 4; i++) {
+        eval("point.push(dx" + i + ")");
+    };
+    for (let i = 0; i < 1; i += 0.08) {
+        xxyy = [parseInt(bezierCurves(point, i).x), parseInt(bezierCurves(point, i).y)];
+        xxy.push(xxyy);
+    }
+    gesture.apply(null, xxy);
+};
+
+function bezierCurves(cp, t) {
+    cx = 3.0 * (cp[1].x - cp[0].x);
+    bx = 3.0 * (cp[2].x - cp[1].x) - cx;
+    ax = cp[3].x - cp[0].x - cx - bx;
+    cy = 3.0 * (cp[1].y - cp[0].y);
+    by = 3.0 * (cp[2].y - cp[1].y) - cy;
+    ay = cp[3].y - cp[0].y - cy - by;
+
+    tSquared = t * t;
+    tCubed = tSquared * t;
+    result = {
+        "x": 0,
+        "y": 0
+    };
+    result.x = (ax * tCubed) + (bx * tSquared) + (cx * t) + cp[0].x;
+    result.y = (ay * tCubed) + (by * tSquared) + (cy * t) + cp[0].y;
+    return result;
+};
